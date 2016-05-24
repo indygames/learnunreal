c35d1381b09734b5ea635afb3d4f7fa5e24ce259
Scott Posch <scott@indygames.com>
2016-05-24 10:55:20 -0600

    initial commit

---
+++ escape/escape/Source/escape/escapeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "escapeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPE_API AescapeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
